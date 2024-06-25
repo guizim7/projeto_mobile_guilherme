b2b0c9e5f2a7aba323be97322b24213f88d8342d
guizim7 <161588662+guizim7@users.noreply.github.com>
2024-06-25 07:50:55 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,25 +5,22 @@ meuFormulario.addEventListener("submit", function(event) {
 });
 
 
-function VerificarPositivoNegativo() 
+function VerificarPositivoNegativo() {
 
-    if (numero > 0){
-      print("O número é positivo");
-    }    else if (numero < 0) {
-        print("O número é negativo");
-    }    else if (numero< 0){
-        print("O número é zero");
+   let numero = Number(document.getElementById("primeiro_numero").value);
+
+        if (numero < 10){
+          console.log("O número é positivo");
+    }    else if (numero < 10) {
+          console.log("O número é negativo");
+    }    else if (numero < 0){
+          console.log("O número é zero");
     }
 
 
-  let primeiro_numero = Number (("primeiro_numero").value);
-
-  var numero = 10;
-
-  var resultado = VerificarPositivoNegativo(primeiro_numero);
 
   //Aqui vai escrever no input "resultado" valor q esta armazenaddo na variavel "resultado"
-  document.getElementById("resultado").value = resultado;
+  document.getElementById("resultado").value = (resultado);
 
 
 }
