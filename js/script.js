80a289268a95dd1a0f56dd02ab562601a28fd2c2
guizim7 <161588662+guizim7@users.noreply.github.com>
2024-08-07 07:50:30 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,25 +2,40 @@ var meuFormulario = document.getElementById("formulario");
 meuFormulario.addEventListener("submit", function(event) {
   event.preventDefault();
 
+  //Chama a função somar
+  somar();
+
 });
 
 
-function VerificarPositivoNegativo() {
+//Função somar
+/* function somar() {
 
-   let numero = Number(document.getElementById("primeiro_numero").value);
+  //Captura o que está dentro do primeiro input no formulário
+  //Number - Transforma o texto capturado em Numero
+  let primeiro_numero_inteiro = Number(document.getElementById("primeiro_numero_inteiro").value);
 
-        if (numero < 10){
-          console.log("O número é positivo");
-    }    else if (numero < 10) {
-          console.log("O número é negativo");
-    }    else if (numero < 0){
-          console.log("O número é zero");
-    }
+  //Captura o que está dentro do segundo input no formulário
+  //Number - Transforma o texto capturado em Numero
+  let segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value);
 
+  //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
+  //variável "segundo_numero_inteiro"
+  //E a variável "resultado_soma" recebe o valor da soma
+  let resultado_soma = primeiro_numero_inteiro + segundo_numero_inteiro;
 
+  //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_soma"
+  document.getElementById("resultado_soma").value = resultado_soma;
+}
+*/
 
-  //Aqui vai escrever no input "resultado" valor q esta armazenaddo na variavel "resultado"
-  document.getElementById("resultado").value = (resultado);
+let vetor=[];
 
-
+function adicionarValor() {
+  const valor = Number(document.getElementById('tabelaValores')).getElementByTagName('tbody')[0];
+  const celula = novalinha.insertCell(0);
+  celula.text content = valor;
+  document.getElementById('valor').value='';
+  document.getElementById('valor').focus();
+}
 }
