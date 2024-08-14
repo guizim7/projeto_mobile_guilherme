f5215d0d168a2d52ef32fb898632d4df4196a871
guizim7 <161588662+guizim7@users.noreply.github.com>
2024-08-14 08:28:15 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,15 +3,34 @@ let vetor = [];
 
 function adicionarValor() {
     const valor = Number(document.getElementById('valor').value);
+
     if (!isNaN(valor)) {
+
       vetor.push(valor);
 
-      const valor = Number(document.getElementById('tabelaValores')).getElementByTagName('tbody')[0];
-      const novalinha = tabela.insertrow();
-      const celula = novalinha.insertCell(0);
-      celula.textcontent = valor;
+      const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
 
-      document.getElementById('valor').value='';
+      const novalinha = tabela.insertRow();
+      const celula = novalinha.insertCell(0);
+      celula.textContent = valor;
+
+      document.getElementById('valor').value = '';
       document.getElementById('valor').focus();
+
+
     }
 }
+/*function mostrarPares(){
+  const valor = document.getElementsById('mostrarPares').getElementsByTagName('tbody')[0];
+
+  for (let i = 0; i < vetor.length; i++){
+    const valor = vetor[i];
+    if (valor % 2 === 0) {
+      const linha = tabela.insertrow();
+      const celula = linha.insertCell(0);
+      celula.textcontent = valor;
+    }
+  }
+ }
+}
+*/
